1f874c2d9bfe6b7f4ae0881ef96697410b75b01f
Wes Dunn <wes@jungledisk.com>
2020-12-24 15:23:10 -0600

    updates to night before story

--- assets/stories.js
+++ assets/stories.js
@@ -1,31 +1,31 @@
 window.STORY1 =
-  'Twas the night before Christmas, when all through the {{Noun (dwelling):house}}'+ '</br>' +
+  'Twas the night before Christmas, when all through the house'+ '</br>' +
   'Not a creature was stirring, not even a {{Animal:mouse}}.'+ '</br>' +
-  'The stockings were hung by the chimney with care,'+ '</br>' +
+  'The stockings were hung by the {{Part of a house:chimney}} with care,'+ '</br>' +
   'In hopes that St Nicholas soon would be there.'+ '</br>' +
   '</br>'+
   'The children were nestled all snug in their beds,'+ '</br>' +
-  'While visions of sugar-plums {{Action Verb:danced}} in their heads.'+ '</br>' +
-  'And mamma in her ‘kerchief, and I in my {{Article of Clothing:cap}},'+ '</br>' +
-  'Had just settled our brains for a long winter’s nap.'+ '</br>' +
+  'While visions of {{Dessert:sugar-plums}} danced in their heads.'+ '</br>' +
+  'And mamma in her ‘kerchief, and I in my cap,'+ '</br>' +
+  'Had just settled our {{Body part (plural):brains}} for a long winter’s nap.'+ '</br>' +
   '</br>'+
   'When out on the lawn there arose such a {{Noise:clatter}},'+ '</br>' +
   'I sprang from the bed to see what was the matter.'+ '</br>' +
   'Away to the window I flew like a flash,'+ '</br>' +
-  'Tore open the shutters and threw up the {{Noun (thing):sash}}.'+ '</br>' +
+  'Tore open the shutters and threw up the sash.'+ '</br>' +
   '</br>'+
-  'The moon on the breast of the {{Adjective:new-fallen}} snow'+ '</br>' +
+  'The moon on the breast of the {{Adjective:new}}-fallen snow'+ '</br>' +
   'Gave the lustre of mid-day to objects below.'+ '</br>' +
   'When, what to my wondering eyes should appear,'+ '</br>' +
-  'But a miniature {{Mode of Transportation:sleigh}}, and {{Number:eight}} tiny reindeer.'+ '</br>' +
+  'But a miniature {{Mode of transportation:sleigh}}, and eight tiny {{Animal:reindeer}}.'+ '</br>' +
   '</br>'+
-  'With a little old driver, so lively and quick,'+ '</br>' +
+  'With a little old driver, so lively and {{Adjective:quick}},'+ '</br>' +
   'I knew in a moment it must be St Nick.'+ '</br>' +
-  'More rapid than {{Animal (plural):eagles}} his coursers they came,'+ '</br>' +
-  'And he whistled, and shouted, and called them by name!'+ '</br>' +
+  'More rapid than eagles his coursers they came,'+ '</br>' +
+  'And he whistled, and {{Verb (-ed):shouted}}, and called them by name!'+ '</br>' +
   '</br>'+
-  '"Now, Dasher! now, Dancer! now, Prancer and {{Name:Vixen}}!'+ '</br>' +
-  'On, Comet! On, Cupid! on, {{Name:Donner}} and Blitzen!'+ '</br>' +
+  '"Now, Dasher! now, Dancer! now, Prancer and Vixen!'+ '</br>' +
+  'On, Comet! On, Cupid! on, {{Name:Donner}} and {{Name:Blitzen}}!'+ '</br>' +
   'To the top of the porch! to the top of the wall!'+ '</br>' +
   'Now dash away! Dash away! Dash away all!"'+ '</br>' +
   '</br>'+
@@ -35,39 +35,39 @@ window.STORY1 =
   'With the {{Mode of Transportation:sleigh}} full of {{Thing (plural):Toys}}, and St Nicholas too.'+ '</br>' +
   '</br>'+
   'And then, in a twinkling, I heard on the roof'+ '</br>' +
-  'The {{Verb (-ing):prancing}} and pawing of each little hoof.'+ '</br>' +
+  'The prancing and {{Verb (-ing):pawing}} of each little hoof.'+ '</br>' +
   'As I drew in my head, and was turning around,'+ '</br>' +
   'Down the chimney St Nicholas came with a bound.'+ '</br>' +
   '</br>'+
-  'He was dressed all in {{Clothing Material:fur}}, from his head to his foot,'+ '</br>' +
+  'He was dressed all in {{Clothing material:fur}}, from his head to his foot,'+ '</br>' +
   'And his clothes were all tarnished with ashes and soot.'+ '</br>' +
   'A bundle of {{Thing (plural):Toys}} he had flung on his back,'+ '</br>' +
   'And he looked like a peddler, just opening his pack.'+ '</br>' +
   '</br>'+
-  'His eyes-how they {{Adjective (-ed):twinkled}}! his dimples how merry!'+ '</br>' +
-  'His cheeks were like roses, his {{Facial Feature:nose}} like a cherry!'+ '</br>' +
+  'His eyes-how they {{Verb (-ed):twinkled}}! his dimples how merry!'+ '</br>' +
+  'His cheeks were like roses, his nose like a {{Fruit:cherry}}!'+ '</br>' +
   'His droll little mouth was drawn up like a bow,'+ '</br>' +
   'And the beard of his chin was as {{Color:white}} as the snow.'+ '</br>' +
   '</br>'+
   'The stump of a pipe he held tight in his teeth,'+ '</br>' +
-  'And the smoke it encircled his head like a {{Holiday Decoration:wreath}}.'+ '</br>' +
+  'And the smoke it encircled his head like a {{Holiday decoration:wreath}}.'+ '</br>' +
   'He had a broad face and a little round belly,'+ '</br>' +
-  'That shook when he {{Adjective (-ed):laughed}}, like a bowlful of jelly!'+ '</br>' +
+  'That shook when he laughed, like a bowlful of {{Food:jelly}}!'+ '</br>' +
   '</br>'+
-  'He was chubby and plump, a right {{Mood (-y):jolly}} old elf,'+ '</br>' +
-  'And I {{Verb (-ed):laughed}} when I saw him, in spite of myself!'+ '</br>' +
-  'A wink of his eye and a twist of his head,'+ '</br>' +
+  'He was chubby and plump, a right {{Adjective (-y):jolly}} old elf,'+ '</br>' +
+  'And I laughed when I saw him, in spite of myself!'+ '</br>' +
+  'A wink of his eye and a twist of his {{Body part:head}},'+ '</br>' +
   'Soon gave me to know I had nothing to dread.'+ '</br>' +
   '</br>'+
   'He spoke not a word, but went straight to his work,'+ '</br>' +
-  'And filled all the {{Article of Clothing:stockings}}, then turned with a jerk.'+ '</br>' +
-  'And laying his {{Body Part:finger}} aside of his nose,'+ '</br>' +
-  'And giving a nod, up the chimney he rose!'+ '</br>' +
+  'And filled all the {{Clothing (plural):stockings}}, then turned with a jerk.'+ '</br>' +
+  'And laying his finger aside of his nose,'+ '</br>' +
+  'And giving a nod, up the chimney he {{Action (past tense):rose}}!'+ '</br>' +
   '</br>'+
   'He sprang to his {{Mode of Transportation:sleigh}}, to his team gave a whistle,'+ '</br>' +
   'And away they all flew like the down of a thistle.'+ '</br>' +
   'But I heard him exclaim, ‘ere he drove out of sight,'+ '</br>' +
-  '"Merry Christmas to all, and to all a good-{{Time of Day:night}}!"';
+  '"Merry Christmas to all, and to all a good-{{Time of day:night}}!"';
 
 window.STORY2 = 
   'Rudolph, the {{Color:red}}-nosed reindeer'+ '</br>' +
@@ -78,7 +78,7 @@ window.STORY2 =
   'All of the other reindeer'+ '</br>' +
   'used to {{Verb:laugh}} and call him names.'+ '</br>' +
   'They never let {{Adjective:poor}} Rudolph'+ '</br>' +
-  'join in any {{Adjective:reindeer}} games.'+ '</br>' +
+  'join in any reindeer {{Activity (plural):games}}.'+ '</br>' +
   '</br>'+
   'Then one {{Adjective (-y):foggy}} Christmas Eve'+ '</br>' +
   'Santa came to say:'+ '</br>' +
@@ -88,21 +88,21 @@ window.STORY2 =
   'Then all the reindeer loved him'+ '</br>' +
   'as they shouted out with glee,'+ '</br>' +
   'Rudolph the {{Color:red}}-nosed reindeer,'+ '</br>' +
-  'you\'ll go {{Direction:down}} in history';
+  'you\'ll go down in {{Noun:history}}';
 
   window.STORY3 =
   `Frosty the Snowman, was a {{Adjective:jolly}} {{Adjective (feeling):happy}} soul,</br>
-  With a {{Noun:corn cob}} pipe and a {{Noun:button}} nose, and {{Number:two}} eyes made of coal.</br>
+  With a corn cob pipe and a button nose, and {{Number:two}} eyes made of {{Noun (thing):coal}}.</br>
   </br>
-  Frosty the Snowman, is a {{Noun:fairytale}}, they say.</br>
-  He was made of snow, but the children know he came to {{Verb:life}} one day.</br>
+  Frosty the Snowman, is a fairytale, they say.</br>
+  He was made of snow, but the {{Group of people:children}} know how he came to life one {{Time of day:day}}.</br>
   </br>
-  There must have been some magic in that old {{Adjective:silk}} hat they found,</br>
+  There must have been some magic in that old {{Clothing material:silk}} hat they found,</br>
   </br>
-  For when they placed it on his head, he began to {{Verb:dance}} around!</br>
+  For when they placed it on his head, he began to {{Action:dance}} around!</br>
   </br>
-  Oh, Frosty, the Snowman, was as {{Mood:alive}} as he could be;</br>
-  and the children say he could {{Verb:laugh}} and play,</br>
+  Oh, Frosty, the Snowman, was as alive as he could be;</br>
+  and the {{Group of people:children}} say he could {{Verb:laugh}} and play,</br>
   just the same as you and me.</br>
   </br>
   Thumpety thump, thump, thumpety thump, thump,</br>
@@ -111,15 +111,15 @@ window.STORY2 =
   Thumpety thump, thump, thumpety thump, thump,</br>
   over the hills of snow.</br>
   </br>
-  Frosty the Snowman, knew the sun was hot that day,</br>
-  so he said, "Let's {{Verb:run}}, and we'll have some {{Noun:fun}} now, before I melt away."</br>
+  Frosty the Snowman, knew the sun was hot that {{Time of day:day}},</br>
+  so he said, "Let's run, and we'll have some {{Noun (idea):fun}} now, before I melt away."</br>
   </br>
   Down to the {{Place:village}}, with a {{Noun (thing):broomstick}} in his hand,</br>
   Running here and there, all around the square,</br>
   sayin', "Catch me if you can."</br>
   </br>
-  He led them down the {{Place:streets of town}}, right to the traffic cop;</br>
-  and only paused a moment, when he heard him holler, "Stop!"</br>
+  He led them down the streets of town, right to the traffic cop;</br>
+  and only paused a moment, when he heard him holler, "{{Exclamation:Stop}}!"</br>
   </br>
   For Frosty, the Snowman, had to hurry on his way,</br>
-  But he {{Verb (-ed):waved}} goodbye, sayin' "Don't you cry, I'll be back again some day.</br>`
+  But he {{Verb (-ed):waved}} goodbye, sayin' "Don't you cry, I'll be back again some {{Time of day:day}}.</br>`
